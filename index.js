5ed7608735ca9e2bdfda960298afba96c08caeba
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-05-19 16:30:27 +0400

    commitss

--- index.js
+++ index.js
@@ -12,10 +12,28 @@ const command = process.argv[2]
 
 const text = process.argv.slice(3).join(" ")
 
+const status = 'done'
+
 
 
 console.log(`Command: ${command}
 Task: ${text}
     `);
 
+    const id =
+       tasks.length === 0 
+       ? 1
+       : tasks[tasks.length - 1].id + 1
 
+ const object = {
+    id: id,
+    description: text,
+    status: status,
+    createdAt: new Date(),
+    updatedAt: new Date()
+ }   
+
+ const objectGo = JSON.stringify(object)
+ tasks.push(objectGo)
+
+ 
