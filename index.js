d1ebb21d73ff88da73bc016cd61c9be21c40073a
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-05-19 15:42:05 +0400

    commitss

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import process from 'process'
+import fs from 'fs'
+
+
+const file = fs.readFileSync('tasks.json', 'utf-8',)
+console.log(file);
+
+
+
+
+
+
+
+
+
+
 
 const command = process.argv[2]
 
 const text = process.argv.slice(3).join(" ")
+
+
 
 console.log(`Command: ${command}
 Task: ${text}
