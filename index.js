bd07613697d7e5783bf97dc7cb61aec09cf6f4d8
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-05-19 15:32:25 +0400

    commitss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import process from 'process'
 
 const command = process.argv[2]
 
-const text = process.argv[3]
+const text = process.argv.slice(3).join(" ")
 
 console.log(`Command: ${command}
 Task: ${text}
